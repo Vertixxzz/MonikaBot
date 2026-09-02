9816cdc5f8761fe6ebfdee4f1ee40598a8f1ed02
vertixxzz <sasuk228e@gmail.com>
2026-09-02 22:42:17 +0500

    pool надо в ловеркейсе писать

--- yuri/handlers/buykey.py
+++ yuri/handlers/buykey.py
@@ -5,13 +5,28 @@ from common.db.economics import get_balance, add_balance
 router = Router()
 
 @router.message(F.text.func(lambda t: t and t.lower().startswith("продай ключ")))
-async def buykey(message: types.Message, Pool):
-    balance = await get_balance(Pool, message.from_user.id)
+async def buykey(message: types.Message, pool):
+    balance = await get_balance(pool, message.from_user.id)
+
     if balance < 1000:
-        await message.reply(f"Я боюсь.. что тебе не хватает докидолларов. Один ключ стоит 1000, у тебя сейчас {balance}")
+        await message.reply(
+            f"Я боюсь.. что тебе не хватает докидолларов. "
+            f"Один ключ стоит 1000, у тебя сейчас {balance}"
+        )
         return
-    await add_balance(Pool, message.from_user.id, message.from_user.username, -1000)
-    await add_key(Pool, message.from_user.id, message.chat.id)
+
+    await add_balance(
+        pool,
+        message.from_user.id,
+        message.from_user.username,
+        -1000
+    )
+
+    await add_key(
+        pool,
+        message.from_user.id,
+        message.chat.id
+    )
 
     await message.reply("Готово! Я добавила тебе один ключ!")
 
